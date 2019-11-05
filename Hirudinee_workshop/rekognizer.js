620e221c6e625b568ac368249a225d68542298a1
HirudineeADRT <hirudinee@adroitlogic.com>
2019-11-05 13:46:29 +0530

    changed permission

--- Hirudinee_workshop/rekognizer.js
+++ Hirudinee_workshop/rekognizer.js
@@ -20,5 +20,5 @@ exports.handler = function(event, context, callback) {
     .catch(callback);
 
 
-    callback(null, {"message": "Successfully executed"});
+    //callback(null, {"message": "Successfully executed"});
 }
