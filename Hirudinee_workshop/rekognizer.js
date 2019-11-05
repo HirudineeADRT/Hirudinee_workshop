1608f73e93eaeca473a644385d13892d8eae10bb
HirudineeADRT <hirudinee@adroitlogic.com>
2019-11-05 13:25:58 +0530

    change

--- Hirudinee_workshop/rekognizer.js
+++ Hirudinee_workshop/rekognizer.js
@@ -1,6 +1,23 @@
+const  AWS = require('aws-sdk');
+const rekog = new AWS.Rekognition();
+
 exports.handler = function(event, context, callback) {
     console.log(event)
-
+let s3 = event.Records[0].s3;
+    rekog.detectLabels({
+        Image: {
+            S3Object: {
+                Bucket: s3.bucket.name,
+                Name:  s3.object.key
+            }
+        },
+        MaxLabels: 1
+    }).promise()
+    .then(data => {
+        console.log(data);
+        callback(null, {});
+    })
+    .catch(callback);
 
 
     callback(null, {"message": "Successfully executed"});
